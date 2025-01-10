573d47b44970bf68ba0a9b82bc7b421f614db088
zoumas <zoumas@protonmail.com>
2025-01-10 22:11:59 +0200

    modules - constants

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
+import constants
 
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":

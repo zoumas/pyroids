a9700488f8c960d7f9a8f1de4323746ee8211740
zoumas <zoumas@protonmail.com>
2025-01-11 00:55:28 +0200

    draw player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+from player import Player
 
 
 def main():
@@ -12,15 +13,15 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
     running = True
-
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
         screen.fill("black")
-
+        player.draw(screen)
         pygame.display.flip()
         # limits FPS to 60
         dt = clock.tick(60) / 1_000

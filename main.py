aac1db05141f9fe1c554158ff20802ef15d44349
zoumas <zoumas@protonmail.com>
2025-01-10 23:27:30 +0200

    game loop

--- main.py
+++ main.py
@@ -3,9 +3,25 @@ import constants
 
 
 def main():
+    pygame.init()
+
     print("Starting asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("black")
+
+        pygame.display.flip()
+
+    pygame.quit()
 
 
 if __name__ == "__main__":

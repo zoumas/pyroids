c465a9d0418da3898a73c9e668933e9fd81135e6
zoumas <zoumas@protonmail.com>
2025-01-11 17:06:11 +0200

    moving forward and backward

--- player.py
+++ player.py
@@ -14,6 +14,13 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_ESCAPE] or keys[pygame.K_q]:
+            pygame.event.post(pygame.event.Event(pygame.QUIT))
+            return
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.move(-dt)
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             self.rotate(dt)
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
@@ -21,6 +28,10 @@ class Player(CircleShape):
 
     def rotate(self, dt):
         self.rotation += constants.PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * constants.PLAYER_SPEED * dt
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)

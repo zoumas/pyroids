19a6dbe50590d90cf5f207d74c964d457aba20b8
zoumas <zoumas@protonmail.com>
2025-09-21 21:46:28 +0300

    basic game loop
    Create a screen using constants for the height and width.
    Fill the screen with a solid black color.
    Refresh the screen.
    Handle quit events.

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
+    pygame.init()
+    screen_size = (constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT)
+    screen = pygame.display.set_mode(screen_size)
+
+    # game loop:
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("QUIT")
+                return
+
+        screen.fill("black")
+
+        # refresh the screen
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()

e1ce71dc457ead87306fde3f33c139e6ebec2a48
zoumas <zoumas@protonmail.com>
2026-04-18 14:25:10 +0300

    2.1: Constants

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
+import constants
 
 
 def main():
     print("Starting Asteroids with pygame version:", pygame.version.ver)
+    print("Screen width:", constants.SCREEN_WIDTH)
+    print("Screen height:", constants.SCREEN_HEIGHT)
 
 
 if __name__ == "__main__":

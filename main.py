9f928396a7b6565b11b461a12a003cfbfea3c082
zoumas <zoumas@protonmail.com>
2026-04-18 14:50:13 +0300

    2.3: Game Loop

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import pygame
 import constants
+from logger import log_state
 
 
 def main():
     print("Starting Asteroids with pygame version:", pygame.version.ver)
     print("Screen width:", constants.SCREEN_WIDTH)
     print("Screen height:", constants.SCREEN_HEIGHT)
+
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
